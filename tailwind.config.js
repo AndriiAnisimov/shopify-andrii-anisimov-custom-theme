e6ce32b2aa271e916a2cdcbab8e2478b97b3bac1
andrii-anisimov <andrii.anisimov@dresslerconsulting.com>
2025-11-04 12:26:03 +0200

    fix: path for tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const path = require('path');
+
 module.exports = {
     content: [
-        './**/*.liquid',
-        './src/**/*.{js,css}',
+        path.join(__dirname, './layout/**/*.liquid'),
+        path.join(__dirname, './templates/**/*.liquid'),
+        path.join(__dirname, './sections/**/*.liquid'),
+        path.join(__dirname, './snippets/**/*.liquid'),
+        path.join(__dirname, './src/**/*.{js,css}'),
     ],
     theme: {
         extend: {},
