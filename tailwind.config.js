d4865ba9f5a092d6bbdd182bf3a88e3c76ff9e6a
andrii-anisimov <andrii.anisimov@dresslerconsulting.com>
2025-11-04 12:21:26 +0200

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
     content: [
-        './layout/**/*.liquid',
-        './sections/**/*.liquid',
-        './snippets/**/*.liquid',
-        './templates/**/*.liquid',
-        './src/**/*.{js,jsx,ts,tsx}'
+        './**/*.liquid',
+        './src/**/*.{js,css}',
     ],
     theme: {
         extend: {},
