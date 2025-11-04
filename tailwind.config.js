08e6b969c001be2c8f47a7a69119c01cc9222a90
andrii-anisimov <andrii.anisimov@dresslerconsulting.com>
2025-11-04 12:40:03 +0200

    fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-const path = require('path');
-
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
     content: [
-        path.join(__dirname, './layout/**/*.liquid'),
-        path.join(__dirname, './templates/**/*.liquid'),
-        path.join(__dirname, './sections/**/*.liquid'),
-        path.join(__dirname, './snippets/**/*.liquid'),
-        path.join(__dirname, './src/**/*.{js,css}'),
+        './layout/**/*.liquid',
+        './templates/**/*.liquid',
+        './sections/**/*.liquid',
+        './snippets/**/*.liquid',
+        './src/**/*.{js,css}',
     ],
     theme: {
         extend: {},
